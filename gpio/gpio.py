bf95ad0f375f8c95663dff84db3c54cd3c2b860b
Konstantin <buecheko@protonmail.com>
2021-01-09 23:42:57 +0100

    Added gpio example

---
+++ gpio/gpio.py
@@ -0,0 +1,43 @@
+from nmigen import Elaboratable, Signal, Module
+from nmigen.lib.io import Pin
+from nmigen_boards.icebreaker import ICEBreakerPlatform
+from nmigen.build import Resource, Pins, Attrs
+
+
+class HelloWorld(Elaboratable):
+    def __init__(self, maxperiod):
+        self.maxperiod = maxperiod
+        self.counter = Signal(range(maxperiod))
+        self.gpio = Pin(1, "o")
+        self.led = Pin(1, "o")
+
+    def elaborate(self, platform):
+        m = Module()
+
+        if platform is not None:
+            self.led = platform.request("led_r")
+            self.gpio = platform.request("gpio")
+
+            with m.If(self.counter == 0):
+                m.d.sync += [self.led.eq(~self.led),
+                             self.gpio.eq(~self.gpio),
+                             self.counter.eq(self.maxperiod)]
+            with m.Else():
+                m.d.sync += self.counter.eq(self.counter - 1)
+        return m
+
+
+if __name__ == "__main__":
+    # This example shows how a single pin can be added
+    # to the resources to use it as GPIO.
+    dut = HelloWorld(3000000)
+    p = ICEBreakerPlatform()
+
+    # Add GPIO pin to resources
+    p.add_resources([
+        Resource("gpio",
+                 0,
+                 Pins("4", dir="o"),
+                 Attrs(IO_STANDARD="SB_LVCMOS"))
+            ])
+    p.build(dut)
